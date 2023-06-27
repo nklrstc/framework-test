fbf1dafc2ef8e9219d7d182d4f027c7201ebcd8d
Nikola Ristic <nristic@equinoxpayments.com>
2023-06-27 18:48:40 +0200

    test

---
+++ framework-test/framework_test.h
@@ -0,0 +1,18 @@
+//
+//  framework_test.h
+//  framework-test
+//
+//  Created by Nenad Lazovic on 2023-06-27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for framework_test.
+FOUNDATION_EXPORT double framework_testVersionNumber;
+
+//! Project version string for framework_test.
+FOUNDATION_EXPORT const unsigned char framework_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <framework_test/PublicHeader.h>
+
+
